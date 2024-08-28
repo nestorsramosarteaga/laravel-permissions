e8123d3173c57c1457c4a69e7c02b566cc757242
Nestor Ramos <nestorsramosarteaga@gmail.com>
2024-08-28 18:42:04 -0500

    Use boottrap instead of tailwind in Default Compnents

--- resources/views/components/dropdown-link.blade.php
+++ resources/views/components/dropdown-link.blade.php
@@ -1,1 +1,2 @@
-<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
+<a
+   {{ $attributes->merge(['class' => 'd-block w-100 px-3 py-2 text-start fs-6 lh-sm text-secondary hover:bg-light focus:bg-light transition']) }}>{{ $slot }}</a>
